802be850f9c4e01a1e8ddbffb9dc6b7124baa98a
oramadn <omar.ramadanpersonal@gmail.com>
2024-08-06 13:51:57 +0300

    React.js Section 3 finished

--- react_tutorial/01-starting-project/src/components/TabButton.jsx
+++ react_tutorial/01-starting-project/src/components/TabButton.jsx
@@ -1,11 +1,7 @@
-export default function TabButton({ children }) {
-  function handleClick() {
-    console.log("Hello World!");
-  }
-
+export default function TabButton({ children, onSelect, isSelected }) {
   return (
     <li>
-      <button onClick={handleClick}>{children}</button>
+      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
     </li>
   );
 }
